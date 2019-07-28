32f44b72f82c68f3b2d38ed302294fb6be67ec94
zz-782824 <zz-782824@derby.ac.uk>
2019-07-28 10:32:08 +0100

    Moved DC1 public IP into DC1 module from main

--- modules/DC1/1-domain-controller.tf
+++ modules/DC1/1-domain-controller.tf
@@ -2,6 +2,14 @@ locals{
     virtual_machine_name  = "DC1"
 }
 
+# Public IP
+resource "azurerm_public_ip" "dc1-publicip" {
+    name                         = "dc1-pip"
+    location                     = "uksouth"
+    resource_group_name          = "${var.resource_group_name}"
+    public_ip_address_allocation = "dynamic"
+}
+
 # DC1 NIC
 resource "azurerm_network_interface" "dc1_nic" {
     name                      = "dc1_nic"
@@ -13,7 +21,7 @@ resource "azurerm_network_interface" "dc1_nic" {
         name                          = "dc1"
         subnet_id                     = "${var.subnet_id}"
         private_ip_address_allocation = "dynamic"
-        public_ip_address_id          = "${var.public_ip_id}"
+        public_ip_address_id          = "${azurerm_public_ip.dc1-publicip.id}"
     }
 }
 
